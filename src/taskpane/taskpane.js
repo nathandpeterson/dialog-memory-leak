2048cc29479433be1f416b28ce13d282a386e2eb
Nathan Peterson <natperson@gmail.com>
2019-07-23 16:18:52 -0700

    add-in opens and closes dialog

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -7,12 +7,33 @@ const DIALOG_URL = 'https://dialog-opener.surge.sh/dialog.html'
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
   }
 });
 
+
+
 export async function run() {
-  Office.context.ui.displayDialogAsync(DIALOG_URL)
+  Office.context.ui.displayDialogAsync(
+    DIALOG_URL, 
+    { height: 30, width: 20, displayInIframe: true },
+    (asyncResult) => {
+      if(asyncResult) {
+        console.log('adding event handler')
+        const dialog = asyncResult.value
+
+        function processMessage(arg) {
+          var messageFromDialog = JSON.parse(arg.message);
+          if(messageFromDialog.dialog === false) {
+            dialog.close()
+          }
+        }
+        dialog.addEventHandler(Office.EventType.DialogMessageReceived, processMessage);
+
+      } else {
+        console.log('asyncResult failed check', asyncResult)
+      }
+      
+    }
+    )
 }
